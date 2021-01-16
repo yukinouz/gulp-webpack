21c256f6b47ef9394765a3fd2d0cd55ca9109726
野瀬友岐 <karl.vanc0uver1st@gmail.com>
2021-01-16 15:51:43 +0900

    [update]chache busting
    css / js にcahce bustingを付与

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { parallel, series, watch } = require("gulp");
+const { parallel, series, watch, src, dest } = require("gulp");
 
 const { bundleJs } = require("./tasks/bundle-js");
 const { useCompileSass } = require("./tasks/compile-sass");
@@ -6,7 +6,18 @@ const { useCompileEjs } = require("./tasks/compile-ejs");
 const { buildServer } = require("./tasks/build-server");
 const { browserReload } = require("./tasks/browser-reload");
 
+const crypto = require('crypto'); 
+const hash = crypto.randomBytes(8).toString('hex');
+const replace = require("gulp-replace");
+
 const path = "./src";
+
+const cache = done => {
+  src('./index.html')
+  .pipe(replace(/\.(js|css)\?rev/g, '.$1?rev='+hash))
+  .pipe(dest("./"));
+  done();
+}
 
 const watchRules = {
   scss: `${path}/scss/**/*.scss`,
@@ -29,5 +40,7 @@ const watchFiles = () => {
 
 exports.sass = compileSass;
 exports.ejs = compileEjs;
+exports.cache = cache;
 exports.bundle = bundleJs;
+exports.build = (compileEjs, compileSass, cache);
 exports.default = parallel(buildServer, watchFiles);
